c11ee266e674afe36b339a7850d229e956fe3d61
Joseph Renan Garcia <jay1g@ai-pros.com>
2018-07-29 18:20:17 +0800

    Added encode to output[url] for mongo insert

--- bsoup/scraper/rappler.py
+++ bsoup/scraper/rappler.py
@@ -32,7 +32,7 @@ def parse_body(content, URL):
     output['images'] = [tag['data-original'] for tag in content.findAll('img')]
     output['tags'] = [encode(tag.getText()) for tag in tags.findAll('a')]
     output['text'] = encode(text)
-    output['url'] = URL
+    output['url'] = encode(URL)
     output['timestamp'] = timestamp.getText().replace('Published ', '').strip()
 
     return output
